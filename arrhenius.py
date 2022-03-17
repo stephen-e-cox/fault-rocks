9f68d7359ad699ebd1a66b23b4a1b8065c93127a
Genevieve C <g.coffey@gns.cri.nz>
2022-03-17 15:21:23 +1300

    added calculations for diffusivity and Arrhenius plots

--- arrhenius.py
+++ arrhenius.py
@@ -19,6 +19,7 @@ temp_pattern = ['temp', 'T ']
 esc_lst = [regex.escape(s) for s in temp_pattern]
 temp_pattern = '|'.join(esc_lst)
 
+
 temp_cols = [col for col in diff_df.columns if regex.search(temp_pattern, col, regex.IGNORECASE)]
 
 try:
@@ -26,6 +27,10 @@ try:
 except IndexError:
     raise MissingColumnError("No temperature columns found")
 
+
+#%%
+
+#section
 # if len(temp_cols) > 1:
 '''It will be very common to have multiple temperature columns, either because someone did a C to K 
 conversion in Excel, because they calculated inverse temperatures, or because the data file has other
@@ -95,4 +100,85 @@ for gas in gas_cols:
     diff_df[gas + " Cumulative Fraction"] = diff_df[gas + " Fraction"].cumsum()
 
 
+#%%
+
+'''Use equations 5a - 5c from Fechtig and Kalbitzer to calculate D for each gas'''
+
+for gas in gas_cols:
+    D = []
+    count = 0
+    for x in diff_df[gas + ' Cumulative Fraction']:
+        F = diff_df[gas + ' Cumulative Fraction']
+        t = diff_df['Time (hr)'] * 60 * 60
+        
+        '''For the first step where F_prior = 0'''
+        if x <= 0.1 and count == 0:
+            F_prior = 0
+            D_first = (F[count]**2 * np.pi)/(36 * t[count])
+            D.append(D_first)
+            count = count + 1
+        
+            '''For fraction dg <= 10% (5a)'''
+        elif x <= 0.1:
+            F_prior = F[count - 1]
+            D_step = ((F[count]**2 - F_prior**2) * np.pi)/(36 * t[count])
+            D.append(D_step)
+            count = count + 1
+            
+            ''' For fraction dg > 10% but less than 90% (5b)'''
+        elif x < 0.9:
+            F_prior = F[count - 1]
+            D_step = 1/(np.pi**2 * t[count]) * (-1 * (np.pi**2)/3 * (F[count] - F_prior) 
+                                                - 2 * np.pi * (np.sqrt(1 - np.pi/3 * F[count]) 
+                                                               - np.sqrt(1 - np.pi/3 * F_prior)))
+            count = count + 1
+            D.append(D_step)
+            
+            ''' For fraction dg >= 90% (5c)'''
+        else:
+            F_prior = F[count - 1]
+            D_step = 1/(np.pi**2 * t[count]) * np.log((1 - F_prior)/(1 - F[count]))
+            print(F[count])
+            count = count + 1 
+            D.append(D_step)
+            
+            
+    diff_df[gas + ' Diffusivity'] = D
+
+#%% Make Arrhenius plots
+
+from numpy.polynomial.polynomial import polyfit
+
+plot_inverse_temp = diff_df['Inverse Temp (10000/K)'].astype(float)
+y_int = [];
+grad = [];
+
+for gas in gas_cols:
+    
+    
+    diff_plot = diff_df[gas + ' Diffusivity']
+    log_diff_plot = np.log(diff_plot)
+    
+    ''' find and ignore steps with complete degassing is reached, to avoid NaNs '''
+    ''' when calculating diffusivity and line of best fit '''
+    
+    end_idx = (diff_df[gas + ' Cumulative Fraction'] > 0.9999).idxmax()
+    
+    ''' find line of best fit '''
+    b, m = polyfit(plot_inverse_temp[0:end_idx], log_diff_plot[0:end_idx] , 1)
+    y_int.append(b)
+    grad.append(m)
+    
+    plt.scatter(plot_inverse_temp[0:end_idx], log_diff_plot[0:end_idx])
+    plt.plot(plot_inverse_temp[0:end_idx], b + m * plot_inverse_temp[0:end_idx], '-')
+    
+    plt.ylabel('ln D/$\mathregular{a^{2}}$')
+    plt.xlabel('10000/T ($\mathregular{K^{-1}}$)')
+    plt.title('Arrhenius plot - ' + gas)
+    plt.show()
+    
+    print(gas + ' y-intercept = ' + str(b))
+    print(gas + ' gradient = ' + str(m))
+
+    
 
